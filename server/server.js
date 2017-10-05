ff47e8ac6bfe1251dacbf7494ba2068094ca2b6b
junior.pena <jtamayodev@gmail.com>
2017-10-05 10:41:00 -0400

    Add mongoose

---
+++ server/server.js
@@ -0,0 +1,52 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp', {
+    useMongoClient: true
+});
+
+// var Todo = mongoose.model('Todo', {
+//     text: {
+//         type: String,
+//         required: true,
+//         minlength: 1,
+//         trim: true
+//     },
+//     completed: {
+//         type: Boolean,
+//         default: false
+//     },
+//     completedAt: {
+//         type: Number,
+//         default: null
+//     }
+// });
+
+// var newTodo = new Todo({
+//     text:'  Edit this video  '
+// });
+
+// newTodo.save().then((doc) => {
+//     console.log('Saved todo', doc);
+// }, (err) => {
+//     console.log('Unable to save todo', err);
+// });
+
+var User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1
+    }
+});
+
+var user = new User({
+    email: 'jtamayo@gmail.com'
+});
+
+user.save().then((doc) => {
+    console.log('User saved', doc);
+}, (err) => {
+    console.log('Unable to save user', err);
+})
